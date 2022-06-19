bebbceb5c45737152ed352602506c9b3d1403e60
Samantha <19739806+UltimateNova1203@users.noreply.github.com>
2022-06-18 22:51:51 -0400

    Update rhel-media.sh

--- rhel-media.sh
+++ rhel-media.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 # Check if root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 # Docker setup
-echo "Adding Docker Repo"
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-echo ""
-echo "Updating DNF"
-dnf update -y
-echo ""
-echo "Installing Docker"
-dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y --allowerasing
-echo ""
-echo "Enabling Docker"
-systemctl enable --now docker
+if [ -f "/usr/bin/docker" ]; then
+    echo "Docker is already installed"
+else
+    echo "Adding Docker Repo"
+    dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    echo ""
+    echo "Updating DNF"
+    dnf update -y
+    echo ""
+    echo "Installing Docker"
+    dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y --allowerasing
+    echo ""
+    echo "Enabling Docker"
+    systemctl enable --now docker
+fi
 
 # NFS Setup
 echo ""
